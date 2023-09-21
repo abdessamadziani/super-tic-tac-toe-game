cd0a210b409a8583cebbd0b8c61bd971e39c3e3f
abdssamad <zianiabdssamad30@gmail.com>
2023-09-21 16:22:12 +0100

    logic done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const resetBtn=document.getElementById('reset')
 const gimeboard = document.querySelector(".gameboard");
-let acceptvalues=[]
+const array = new Array(400).fill('');
 
 for (let i = 0; i < 400; i++) {
     // Create a new div element
@@ -20,12 +20,14 @@ for (let i = 0; i < 400; i++) {
 
 
 player1={
-    symbol: '<i class="fas fa-times"></i>',
+    // symbol: '<i class="fas fa-times"></i>',
+    symbol: 'x',
     played: [],
     score: 0
 }
 player2={
-    symbol: '<i class="far fa-circle"></i>',
+    // symbol: '<i class="far fa-circle"></i>',
+    symbol: 'o',
     played:[],
     score: 0
 }
@@ -53,7 +55,7 @@ for(let i=0;i<400;i++)
             if(playerturn)
             {
                  addSymbol(player1,i)
-                //  checkWinner(player1)
+                 checkWinner(player1,i)
                  console.log( 'p1'+player1.played)
                  playerturn=false  
             }
@@ -78,21 +80,124 @@ for(let i=0;i<400;i++)
 function addSymbol(player,i)
 {
     cells[i].innerHTML=player.symbol
+    console.log(i)
     player.played.push(i)
     usedCells.push(i)
+    array[i]=player.symbol
 }
 
 
 
 
-function checkWinner(player)
+// function checkWinner(player)
+// {
+//     winCombos.some(combo=>{
+//         if(combo.every(index=>player.played.includes(index)))
+//         {  alert('Won')  }
+//     })
+
+// }
+const lastCaseRow=[];
+const firstCaseRow=[];
+
+for(let j=19;j<400;j+=20)
 {
-    winCombos.some(combo=>{
-        if(combo.every(index=>player.played.includes(index)))
-        {  alert('Won')  }
-    })
-
+    lastCaseRow.push(j)
 }
+for(let j=0;j<400;j+=20)
+{
+    firstCaseRow.push(j)
+}
+console.log(lastCaseRow)
+console.log(firstCaseRow)
+function checkWinner(player,i)
+{
+    
+
+    //let isValueInArray = firstCaseRow.includes(i || i-1 || i-2|| i-3 || i -4);
+    let isValueInArray=false;
+    let isValueInArray2 = false;
+
+    for (let j = i; j > i - 4; j--) {
+        if (firstCaseRow.includes(j)) {
+            isValueInArray2 = true;
+            break;
+        }
+    }
+
+    for (let j = i; j < i + 4; j++) {
+        if (lastCaseRow.includes(j)) {
+            isValueInArray = true;
+            break;
+        }
+    }
+
+ 
+   
+        if(i+4<400 && !isValueInArray) 
+        {
+
+            console.log('i can enter + ')
+
+            if(cells[i+1].innerHTML==='x' && cells[i+2].innerHTML==='x' && cells[i+3].innerHTML==='x' && cells[i+4].innerHTML==='x') 
+            alert(' THE WINNER IS X ')
+        }
+        if(i-4>=0 && !isValueInArray2)
+        {
+            console.log('i can enter - ')
+            if(cells[i-1].innerHTML==='x' && cells[i-2].innerHTML==='x' && cells[i-3].innerHTML==='x' && cells[i-4].innerHTML==='x' ) 
+            alert(' THE WINNER IS X ')
+        }
+        // if(cells[i].innerHTML==='x' && i+4<400 && i+80<400 && i-4>=0 && i-80>=0)
+        if(cells[i].innerHTML==='x' && i+21<399 && i+19<399 )
+
+        {
+            if(cells[i+1].innerHTML==='x' && cells[i+2].innerHTML==='x' && cells[i-1].innerHTML==='x' && cells[i-2].innerHTML==='x')
+            alert(' THE WINNER IS X ')
+            if(cells[i+20].innerHTML==='x' && cells[i+40].innerHTML==='x' && cells[i-20].innerHTML==='x' && cells[i-40].innerHTML==='x')
+            alert(' THE WINNER IS X ')
+            if(cells[i+21].innerHTML==='x' && cells[i+42].innerHTML==='x' && cells[i-21].innerHTML==='x' && cells[i-42].innerHTML==='x')
+            alert(' THE WINNER IS X mm')
+            if(cells[i+19].innerHTML==='x' && cells[i+38].innerHTML==='x' && cells[i-19].innerHTML==='x' && cells[i-38].innerHTML==='x')
+            alert(' THE WINNER IS X tt ')
+
+        } 
+
+        if(i+80<400)
+        {
+            if(cells[i+20].innerHTML==='x' && cells[i+40].innerHTML==='x' && cells[i+60].innerHTML==='x' && cells[i+80].innerHTML==='x' )
+            alert(' THE WINNER IS X ')
+        }
+        if(i-80>=0)
+        {
+            if(cells[i-20].innerHTML==='x' && cells[i-40].innerHTML==='x' && cells[i-60].innerHTML==='x' && cells[i-80].innerHTML==='x')
+            alert(' THE WINNER IS X ')
+        }
+         if(i+84<400)
+         {
+            if(cells[i+20+1].innerHTML==='x' && cells[i+40+2].innerHTML==='x' && cells[i+60+3].innerHTML==='x' && cells[i+80+4].innerHTML==='x')
+            alert(' THE WINNER IS X ')
+         }
+         if(i-84>=0)
+         {
+            if(cells[i-20-1].innerHTML==='x' && cells[i-40-2].innerHTML==='x' && cells[i-60-3].innerHTML==='x' && cells[i-80-4].innerHTML==='x')
+            alert(' THE WINNER IS X ')
+         }
+         if(i+76<400)
+         {
+            if(cells[i+19].innerHTML==='x' && cells[i+38].innerHTML==='x' && cells[i+57].innerHTML==='x' && cells[i+76].innerHTML==='x')
+            alert(' THE WINNER IS X ')
+         }
+         if(i-76>=0)
+         {
+            if(cells[i-19].innerHTML==='x' && cells[i-38].innerHTML==='x' && cells[i-57].innerHTML==='x' && cells[i-76].innerHTML==='x')
+            alert(' THE WINNER IS X ')
+         }
+   
+}
+
+
+
 function isEmpty(i)
 {
     return usedCells.includes(i) ? false : true
